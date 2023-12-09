b2f762a976f7ae046881e9bf37737cc172f7e971
salashiram <hiramsalas@192.168.1.10>
2023-12-09 02:43:02 -0600

    cambios 2.2.0.15

--- Models/menu.php
+++ Models/menu.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
 if (isset($_SESSION['usuario'])) {
-    if (!isset($_SESSION['showGrades'])) {
-        $_SESSION['showGrades'] = true; 
-    }
-    if (!isset($_SESSION['showCourses'])) {
-        $_SESSION['showCourses'] = true; 
-    }
-    if (!isset($_SESSION['showKardex'])) {
-        $_SESSION['showKardex'] = false; 
-    }
-    if (isset($_SESSION['usuario'])) {
-        echo ' <div class="menu_content">
+  if (!isset($_SESSION['showGrades'])) {
+    $_SESSION['showGrades'] = true;
+  }
+  if (!isset($_SESSION['showCourses'])) {
+    $_SESSION['showCourses'] = true;
+  }
+  if (!isset($_SESSION['showKardex'])) {
+    $_SESSION['showKardex'] = false;
+  }
+  if (isset($_SESSION['usuario'])) {
+    echo ' <div class="menu_content">
         <ul class="menu_items">
           <li class="menu_item" id="homeButton">
             <a href="../Views/home.html" class="nav_link">
@@ -21,10 +21,10 @@ if (isset($_SESSION['usuario'])) {
               <span class="navlink">Home</span>
             </a>
           </li>';
-    }
+  }
 
-    if ($_SESSION['showGrades']) {
-        echo '<li class="menu_item" id="gradesButton">
+  if ($_SESSION['showGrades']) {
+    echo '<li class="menu_item" id="gradesButton">
         <a href="../Views/misCalificaciones.html" class="nav_link">
           <span class="navlink_icon">
             <i class="bx bx-cloud-upload"></i>
@@ -32,10 +32,10 @@ if (isset($_SESSION['usuario'])) {
           <span class="navlink">Mis calificaciones</span>
         </a>
       </li>';
-    }
+  }
 
-    if ($_SESSION['showCourses']) {
-        echo ' <li class="menu_item" id="coursesButton">
+  if ($_SESSION['showCourses']) {
+    echo ' <li class="menu_item" id="coursesButton">
         <a href="../Views/misCursos.html" class="nav_link">
           <span class="navlink_icon">
             <i class="bx bx-cloud-upload"></i>
@@ -43,10 +43,10 @@ if (isset($_SESSION['usuario'])) {
           <span class="navlink">Mis cursos</span>
         </a>
       </li>';
-    }
+  }
 
-    if ($_SESSION['showKardex']) {
-        echo '<li class="menu_item" id="kardexButton">
+  if ($_SESSION['showKardex']) {
+    echo '<li class="menu_item" id="kardexButton">
         <a href="../Views/kardex.html" class="nav_link">
           <span class="navlink_icon">
             <i class="bx bx-cloud-upload"></i>
@@ -54,10 +54,10 @@ if (isset($_SESSION['usuario'])) {
           <span class="navlink">Mi Kardex</span>
         </a>
       </li>';
-    }
+  }
 
 
-    echo '   
+  echo '   
           <li class="menu_item" id="settingsButton">
             <a href="../Views/configuracion.html" class="nav_link">
               <span class="navlink_icon">
@@ -79,7 +79,7 @@ if (isset($_SESSION['usuario'])) {
           <div class="profile_section">
             <div class="profile_info">
               <a href="../Views/userProfile.html">
-                <img src="../Resources/Profile-pics/kimson-doan-HD8KlyWRYYM-unsplash.jpg" alt="Imagen de perfil" class="profile_image" id="profileImage">
+              <img src="../Resources/Profile-pics/blank-profile-picture-973460_960_720.webp" alt="Imagen de perfil" class="profile_image" id="profileImage">
               </a>
               <!-- <p class="user-name" id="correo"></p> -->
             </div>
@@ -107,6 +107,6 @@ if (isset($_SESSION['usuario'])) {
           </div>
         </div>
       </div>';
-      
+
 }
 ?>
