36f8ca6e90bab95a8283327178067467369dcf62
Roberto Ruiz <112975092+CopitoFrostbite@users.noreply.github.com>
2023-12-08 21:20:39 -0600

    botones lateral

--- Models/manejarToggle.php
+++ Models/manejarToggle.php
@@ -2,8 +2,10 @@
 session_start();
 
 // Cambiar el estado de los toggles en la sesión según la solicitud AJAX
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 if (isset($_POST['toggle']) && isset($_POST['state'])) {
-    $toggleState = $_POST['state'] === 'true' ? true : false; // Asegúrate de que el estado se convierte correctamente
+    $toggleState = $_POST['state'] === 'true' ? true : false;
+
     switch ($_POST['toggle']) {
         case 'myToggle4':
             $_SESSION['showGrades'] = $toggleState;
@@ -16,9 +18,20 @@ if (isset($_POST['toggle']) && isset($_POST['state'])) {
             break;
     }
 
-    // Depuración
-    
+    // Devolver los estados actuales
+   
+} else {
+    $response = ['success' => false, 'message' => 'Datos no proporcionados'];
+}
+echo json_encode($response);
+}else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Devolver los estados de los toggles
+    $response = [
+        'showGrades' => $_SESSION['showGrades'] ?? false,
+        'showCourses' => $_SESSION['showCourses'] ?? false,
+        'showKardex' => $_SESSION['showKardex'] ?? false
+    ];
+    echo json_encode($response);
 }
 
-echo json_encode(['success' => true]);
 ?>
